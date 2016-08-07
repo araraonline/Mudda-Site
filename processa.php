2eb58dea7a6f37dbc76fcf8892e5f78f8eaa1824
PedroDominciano <pedrocb82@gmail.com>
2016-08-07 18:26:17 -0300

    Adição de comentarios

--- processa.php
+++ processa.php
@@ -1,6 +1,9 @@
+
+
 <?php
 	include ('conecta.php');
 	
+	//armazena valores dos campos do formulario
     $nome 	= $_POST["nome"] ." ";
 	$nome 	.= $_POST["sobrenome"];
 	$email 	= $_POST["email"];
@@ -9,24 +12,30 @@
 	$time	= time();
 	$data	= date("Y-m-d",$time);
 	$id;
+	
 	// Check connection
 	if ($mysqli_conn->connect_error) {
 	    die("Connection failed: " . $mysqli_conn->connect_error);
-	} 
+	}
 	
+	//inserção de valores na tabela 'messages'
 	$sql = "INSERT INTO messages (date, email, `from`, message, subject, time) 
 			VALUES ('$data', '$email', '$nome', '$campo', '$ass', '$time')";
-	
-	if ($mysqli_conn->query($sql) === TRUE) {
+			
+	//executa inserção caso for TRUE
+	if ($mysqli_conn->query($sql) === TRUE) 
+	{
 		$id = $mysqli_conn->insert_id;
 		for($i=1; $i<=6; $i++){
 			insertID_user($i,$id, $mysqli_conn);
 		}
-	    echo "New record created successfully com $id ";
-	} else {
-	    echo "Error: " . $sql . "<br>" . $mysqli_conn->error;
+	    echo "Gravado com sucesso";
+	}
+	else {
+	    echo "Erro: " . $sql . "<br>" . $mysqli_conn->error;
 	}
 	
+	//função para inserir o ID em todas as tabelas de usuario
 	function insertID_user($num,$id,$mysqli_conn){
 		$usuario = 'usuario'.$num;
 		$sql = "INSERT INTO $usuario (idmessage) 
